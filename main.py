d43b22ea6405d6ccf7dd5e9d884afc5baf77e717
Divyanshusingh134 <divyanshusingh1011@gmail.com>
2026-08-24 20:44:09 +0530

    implemented embedding model with caching embedder

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import logging
 import argparse
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
+from langchain_classic.storage import LocalFileStore
+from langchain_classic.embeddings import CacheBackedEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+store = LocalFileStore("./my_embedding_cache/")
 
 parser = argparse.ArgumentParser(description="RAG Pipeline Evaluator")
 parser.add_argument("--data", nargs='+', required=True, help="path of text file")
@@ -27,13 +31,19 @@ def Load_Split_document(text_file: str) -> list:
     logging.info(f"Successfully split into {len(final_chunks)} chunks.")
     return final_chunks
 
-def genai(chunks: list):
-    embeddings = GoogleGenerativeAIEmbeddings(
-        model="gemini-embedding-2",
-        task_type="RETRIEVAL_DOCUMENT",
-        output_dimensionality=768
-    )
-    return embeddings.embed_documents(chunks)
+# Base embedding model
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2",
+    task_type="RETRIEVAL_DOCUMENT",
+    output_dimensionality=768
+)
+
+cached_embedder = CacheBackedEmbeddings.from_bytes_store(
+    underlying_embeddings=embeddings,
+    document_embedding_cache=store,
+    namespace= embeddings.model
+)
+
 
 # class TextProcessor:
 #     @staticmethod
@@ -55,8 +65,17 @@ class RAGPipeline:
     def __init__(self) -> None:
         pass
     def run_evaluator(self, file_name: list, queries_file: str, outfile: str):
-        final_chunks = Load_Split_document(args.data[0])
-        vectors = genai(final_chunks)
+        all_chunks = []
+        for file in file_name:
+            chunks = Load_Split_document(file)
+            all_chunks.extend(chunks)
+
+        if not all_chunks:
+            logging.error("No chunks loaded.")
+            raise
+        text_content = [chunk.page_content for chunk in all_chunks]
+        vectors = cached_embedder.embed_documents(text_content)
+        logging.info(f"Successfully generated {len(vectors)} vectors.")
         pass
     
 if __name__ == "__main__":
