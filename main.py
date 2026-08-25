ce56be7b40ae1f802b74d74bf1245d9b88f8ccbf
Divyanshusingh134 <divyanshusingh1011@gmail.com>
2026-08-25 11:56:45 +0530

    reading queries file

--- main.py
+++ main.py
@@ -12,63 +12,77 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-store = LocalFileStore("./my_embedding_cache/")
-
-parser = argparse.ArgumentParser(description="RAG Pipeline Evaluator")
-parser.add_argument("--data", nargs='+', required=True, help="path of text file")
-parser.add_argument("--queries", help="path of queries file")
-parser.add_argument("--output", nargs='+', help="path of output file")
-args = parser.parse_args()
-
-def Load_Split_document(text_file: str) -> list:
-    loader = TextLoader(file_path=text_file)
-    document = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 200
-    )
-
-    final_chunks = text_splitter.split_documents(documents=document)
-    logging.info(f"Successfully split into {len(final_chunks)} chunks.")
-    return final_chunks
-
 # Base embedding model
-embeddings = GoogleGenerativeAIEmbeddings(
-    model="gemini-embedding-2",
-    task_type="RETRIEVAL_DOCUMENT",
-    output_dimensionality=768
-)
-
-cached_embedder = CacheBackedEmbeddings.from_bytes_store(
-    underlying_embeddings=embeddings,
-    document_embedding_cache=store,
-    namespace= embeddings.model
-)
 
 class RAGPipeline:
     def __init__(self):
+        self.store = LocalFileStore("./my_embedding_cache/")
+
+        self.embeddings = GoogleGenerativeAIEmbeddings(
+            model="gemini-embedding-2",
+            task_type="RETRIEVAL_DOCUMENT",
+            output_dimensionality=768
+        )
+
+        self.cached_embedder = CacheBackedEmbeddings.from_bytes_store(
+            underlying_embeddings=self.embeddings,
+            document_embedding_cache=self.store,
+            namespace= self.embeddings.model
+        )
         self.vector_store = Chroma(
             collection_name = "Cricket-INFO",
-            embedding_function=cached_embedder,
-            persist_directory=".//chroma_langchain_db"
+            embedding_function=self.cached_embedder,
+            persist_directory="./chroma_langchain_db"
         )
+
+    def _Load_Split_document(self, text_file: str) -> list:
+        loader = TextLoader(file_path=text_file)
+        document = loader.load()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 200
+        )
+
+        final_chunks = text_splitter.split_documents(documents=document)
+        logging.info(f"Successfully split into {len(final_chunks)} chunks.")
+        return final_chunks
+    
 
     def run_evaluator(self, file_name: list, queries_file: str, outfile: str):
         all_chunks = []
         for file in file_name:
-            chunks = Load_Split_document(file)
+            chunks = self._Load_Split_document(file)
             all_chunks.extend(chunks)
 
         if not all_chunks:
             logging.error("No chunks loaded.")
-            raise
-        ids = [hashlib.md5(chunk[0].page_content.encode("utf-8")).hexdigest() for chunk in all_chunks]
+            raise ValueError("No chunks loaded.")
+        
+        ids = [hashlib.md5(chunk.page_content.encode("utf-8")).hexdigest() for chunk in all_chunks]
         self.vector_store.add_documents(documents=all_chunks, ids=ids)
-        logging.info(f"Successfully added {len(all_chunks)} chunks to Chroma.")
-        pass
+        logging.info(f"Successfully added {self.vector_store._collection.count()} chunks to Chroma.")
+
+        try:
+            with open(queries_file, "r", encoding="utf-8") as file:
+                content = file.readlines()
+        except FileNotFoundError:
+            logging.error(f"File not found at path: {queries_file}")
+            raise
+
+        for i, query in enumerate(content):
+            result = self.vector_store.similarity_search(query=query, k=3)
+            logging.info(f"Successfully generated answer for {i + 1}/{len(content)}.")
+            print(result)
+            break
+
     
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="RAG Pipeline Evaluator")
+    parser.add_argument("--data", nargs='+', required=True, help="path of text file")
+    parser.add_argument("--queries", required=True, help="path of queries file")
+    parser.add_argument("--output", nargs='+', help="path of output file")
+    args = parser.parse_args()
     pipeline = RAGPipeline()
     pipeline.run_evaluator(args.data, args.queries, args.output)
 
